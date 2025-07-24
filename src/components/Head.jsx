5f65d13f6a609caf753bb47d8ae4a9d8c8e07639
SandrineMerci <namahsando@gmail.com>
2025-07-24 08:53:09 -0800

    change head jsx

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,23 +1,25 @@
+import { LayoutDashboard } from "lucide-react";
+
 export default function Head() {
   return (
-<section className="py-6 px-6 bg-[oklch(93.5% 0.02 180)]">
-  <div className="flex items-center justify-between">
-    {/* Left: Logo/Title */}
-    <h2 className="text-2xl font-bold">
-      <span className="text-black">Business </span>
-      <span className="text-[color:var(--primary-600)]">Cafe</span>
-    </h2>
+    <section className="py-6 px-6 bg-white">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-4">
+        
+        {/* Left: Logo/Title */}
+        <h2 className="text-2xl font-bold flex items-center gap-2">
+           <LayoutDashboard color="#00816e" className="w-6 h-6" />
+          <span className="text-black">Business</span>
+          <span className="text-[color:var(--primary-600)]">Cafe</span>
+        </h2>
 
-    {/* Right: Navigation */}
-    <nav className="flex gap-6 text-[oklch(28%_0.09_180)] font-medium text-lg">
-     <a href="#" className="text-[color:var(--primary-600)] hover:underline">Home</a>
-      <a href="#" className="text-black hover:underline">Space</a>
-      <a href="#" className="text-black hover:underline">About</a>
-      <a href="#" className="text-black hover:underline">Contact</a>
-    </nav>
-  </div>
-</section>
-
-
+        {/* Right: Navigation */}
+        <nav className="flex flex-wrap justify-center gap-4 md:gap-6 text-[oklch(28%_0.09_180)] font-medium text-lg">
+          <a href="#" className="text-[color:var(--primary-600)] hover:underline">Home</a>
+          <a href="#" className="text-black hover:underline">Space</a>
+          <a href="#" className="text-black hover:underline">About</a>
+          <a href="#" className="text-black hover:underline">Contact</a>
+        </nav>
+      </div>
+    </section>
   );
 }
